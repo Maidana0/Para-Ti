7ae3e512f508b7a40e6afd03a8f7331eec12eb57
Maidana <franco_maidana07@hotmail.com>
2022-10-01 11:01:46 -0300

    index

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import './App.css';
 import React from 'react';
-import Carousel from './componentes/Carousel';
+import Index from './componentes/Carousel';
 
 function App() {
   return (
-      <div id='index' className='container text-center rounded'>
-       <h1 className='title'> Bienvenidx a nuestra página de entretenimiento!! </h1>
-       <h4 className='text'> 
-       En este sitio podrás obtener sugerencias de series, películas y anime para disfrutar y encontrarlas en sus respectivas plataformas.
-       </h4>
-       <Carousel/>
-       
+      <div id='app'>
+       <Index/>
       </div>
   );
-
 }
 
 export default App;
--- src/componentes/Carousel.js
+++ src/componentes/Carousel.js
@@ -1,47 +1,52 @@
 import React from 'react';
 
-function Carousel() {
+function Index() {
     return (
-<div className='carrusel '>
-  <div id='carouselExampleCaptions' className='carousel slide' data-ride='carousel'>
-    <ol className='carousel-indicators'>
-      <li data-target='#carouselExampleCaptions' data-slide-to='0' className='active'></li>
-      <li data-target='#carouselExampleCaptions' data-slide-to='1'></li>
-      <li data-target='#carouselExampleCaptions' data-slide-to='2'></li>
-    </ol>
-    <div className='carousel-inner '>
-      <div className='carousel-item active'>
-   
-        <img src={require('../imagenes/vikings.jpg')} className='d-block w-100' alt='Series-ParaTi'/>
-        <div className='carousel-caption d-md-block'>
-        <a href='#series'> <h5>SERIES</h5> </a>
+  <div id='index' className='container text-center rounded'>
+    <h1 className='title'> Bienvenidx a nuestra página de entretenimiento!! </h1>
+    <h4 className='text'> En este sitio podrás obtener sugerencias de series, películas y anime para disfrutar y encontrarlas en sus respectivas plataformas. </h4>       
+    <div className='carrusel '>
+      <div id='carouselExampleCaptions' className='carousel slide' data-ride='carousel'>
+        <ol className='carousel-indicators'>
+          <li data-target='#carouselExampleCaptions' data-slide-to='0' className='active'></li>
+          <li data-target='#carouselExampleCaptions' data-slide-to='1'></li>
+          <li data-target='#carouselExampleCaptions' data-slide-to='2'></li>
+        </ol>
+        <div className='carousel-inner '>
+          <div className='carousel-item active'>      
+            <img src={require('../imagenes/vikings.jpg')} className='d-block w-100' alt='Series-ParaTi'/>
+            <div className='carousel-caption d-md-block'>
+            <a href='#series'> <h5>SERIES</h5> </a>
+            </div>
+        
+          </div>
+          <div className='carousel-item'>
+            <img src={require('../imagenes/guason.jpg')} className='d-block w-100' alt='Peliculas-ParaTi'/>
+            <div className='carousel-caption d-md-block'>
+            <a href='#peliculas'> <h5>PELICULAS</h5> </a>
+            </div>
+          </div>
+          
+          <div className='carousel-item'>
+            <img src={require('../imagenes/law.jpg')} className='d-block w-100' alt='Anime-ParaTi'/>
+            <div className='carousel-caption d-md-block'>
+            <a href='#peliculas'> <h5>ANIME</h5> </a>
+            </div>
+          </div>
         </div>
-     
-      </div>
-      <div className='carousel-item'>
-        <img src={require('../imagenes/guason.jpg')} className='d-block w-100' alt='Peliculas-ParaTi'/>
-        <div className='carousel-caption d-md-block'>
-        <a href='#peliculas'> <h5>PELICULAS</h5> </a>
-        </div>
-      </div>
-      <div className='carousel-item'>
-        <img src={require('../imagenes/law.jpg')} className='d-block w-100' alt='Anime-ParaTi'/>
-        <div className='carousel-caption d-md-block'>
-        <a href='#peliculas'> <h5>ANIME</h5> </a>
-        </div>
+
+        <a className='carousel-control-prev' href='#carouselExampleCaptions' role='button' data-slide='prev'>
+          <span className='carousel-control-prev-icon' aria-hidden='true'></span>
+          <span className='sr-only'>Previous</span>
+        </a>
+        <a className='carousel-control-next' href='#carouselExampleCaptions' role='button' data-slide='next'>
+          <span className='carousel-control-next-icon' aria-hidden='true'></span>
+          <span className='sr-only'>Next</span>
+        </a>
       </div>
     </div>
-    <a className='carousel-control-prev' href='#carouselExampleCaptions' role='button' data-slide='prev'>
-      <span className='carousel-control-prev-icon' aria-hidden='true'></span>
-      <span className='sr-only'>Previous</span>
-    </a>
-    <a className='carousel-control-next' href='#carouselExampleCaptions' role='button' data-slide='next'>
-      <span className='carousel-control-next-icon' aria-hidden='true'></span>
-      <span className='sr-only'>Next</span>
-    </a>
-  </div>
-</div>
+  </div>   
     );
   }
   
-export default Carousel;
+export default Index;
